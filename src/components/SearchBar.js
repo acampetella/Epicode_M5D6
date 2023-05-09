060ad6b858f8072752b07625896c4852ca3ca45e
acampetella <71129127+acampetella@users.noreply.github.com>
2023-05-09 22:36:20 +0200

    Add files via upload

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import { useState } from 'react'
@@ -6,13 +6,13 @@ import { useState } from 'react'
 const SearchBar = ({totalBooksList, setBooks}) => {
     const [searchKey, setSearchKey] = useState('')
     const filterBooks = () => {
-      if (searchKey !== '') {
-        setBooks(totalBooksList.filter((book) => 
-        book.title.toLowerCase().includes(searchKey.toLowerCase())))
-      } else {
-        setBooks(totalBooksList)
-      }
+        const filter = totalBooksList.filter((book) => 
+        book.title.toLowerCase().includes(searchKey.toLowerCase()))
+        setBooks(filter)
     }
+    useEffect(() => {
+      filterBooks()
+    }, [searchKey])
   return (
     <Container className='mt-3 fs-4'>
         <Row sm={12}>
@@ -21,7 +21,6 @@ const SearchBar = ({totalBooksList, setBooks}) => {
                 placeholder='Search'
                 onChange={(e) => {
                   setSearchKey(e.target.value)
-                  filterBooks()
                 } }
             />
         </Row>
